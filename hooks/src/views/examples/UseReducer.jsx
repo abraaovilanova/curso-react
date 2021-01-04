3a18599c8a66e46a6fa64d68be76616bfe1cf821
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-01-03 21:18:17 -0300

    useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,5 +1,6 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
 import PageTitle from '../../components/PageTitle'
+import SectionTitle from '../../components/PageTitle'
 
 const initialState = {
     cart: [],
@@ -11,10 +12,18 @@ const initialState = {
 
 function reducer(state, action) {
     switch(action.type){
+        case 'times7':
+            return { ...state, number: state.number * 7 }
+        case 'div25':
+            return { ...state, number: state.number / 25 }
+        case 'parseint':
+            return { ...state, number: parseInt(state.number)}
+        case 'n++':
+            return { ...state, number: state.number + action.payload.n }
         case 'numberAdd2':
             return { ...state, number: state.number + 2 }
         case 'login':
-            return { ...state, user: { name: 'Abraão' } }
+            return { ...state, user: { name: action.payload } }
         default: 
             return state
     }
@@ -24,6 +33,7 @@ function reducer(state, action) {
 const UseReducer = props => {
 
     const [state, dispatch] = useReducer(reducer, initialState)
+    const [numberInput, setNumberInput] = useState(0)
 
     return (
         <div className="UseReducer">
@@ -31,6 +41,7 @@ const UseReducer = props => {
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes" />
             
+            <SectionTitle title="Exercício #01" />
             <div className="center">
                 {state.user ?
                     <span className="text">{state.user.name}</span> : 
@@ -40,14 +51,27 @@ const UseReducer = props => {
                 <div>
                     <button 
                         className="btn"
-                        onClick={() => dispatch({type: 'login', name: 'Maria' }) }>Login</button>
+                        onClick={() => dispatch({type: 'login', payload: 'Maria' }) }>Login</button>
                     <button 
                         className="btn"
                         onClick={() => dispatch({type: 'numberAdd2' }) }>+2</button>
                 </div>
+                
             </div>
-            
-
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <input 
+                    type="number" 
+                    className="input"
+                    value={numberInput}
+                    onChange={(e)=> setNumberInput(e.target.value)}/>
+                <div>
+                    <button className="btn" onClick={()=>dispatch({type: 'times7'})}>*7</button>
+                    <button className="btn" onClick={()=>dispatch({type: 'div25'})}>/25</button>
+                    <button className="btn" onClick={()=>dispatch({type: 'parseint'})}>Int</button>
+                    <button className="btn" onClick={()=>dispatch({type: 'n++', payload: {n: Number(numberInput)}})}>n++</button>
+                </div>
+            </div>
         </div>
     )
 }
