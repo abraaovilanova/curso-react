fbcbd9d1adb023a6c17f9d4ce5b3ffc595e66c06
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-01-13 19:01:18 -0300

    Finalização do backend
    setup do frontend

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,12 +1,17 @@
 const port = 3003
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') // fará o sarse do corpo da requisição
 const express = require('express')
-const server = express()
+const server = express() // start do express
+const allowCors = require('./cors')
 
+ // midware chain of responsibility
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
